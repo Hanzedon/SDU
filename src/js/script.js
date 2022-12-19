b406886ebc4f2314f99723bae55d089e4c6aa74b
Kenesbay Dimash <viport19@gmail.com>
2022-12-20 04:49:09 +0600

    sui

--- src/js/script.js
+++ src/js/script.js
@@ -1,54 +1,283 @@
-let temp1 = [
+    let arrays_goods_pop = [
     {
+        id: 1,
         type: "pant",
-        src: "images/bagi.jpg",
+        src1: "images/bagi.jpg",
         name: "Double Knee Lose-Fit",
-        price: "1235"
+        price: "PRICE: 10$"
+
     },
     {
+        id: 2,
         type: "pant",
-        src: "images/muha.jpg",
+        src1: "images/muha.jpg",
         name: "Double Knee Lose-Fit",
-        price: "1235"
+        price: "PRICE: 20$"
     },
     {
+        id: 3,
         type: "pant",
-        src: "images/muha.jpg",
+        src1: "images/muha.jpg",
         name: "Double Knee Lose-Fit",
-        price: "1235"
+        price: "PRICE: 30$"
     },
     {
+        id: 4,
         type: "pant",
-        src: "images/muha.jpg",
+        src1: "images/muha.jpg",
         name: "Double Knee Lose-Fit",
-        price: "1235"
+        price: "PRICE: 40$"
     },
     {
+        id: 5,
         type: "pant",
-        src: "images/sdu.jpg",
+        src1: "images/maha_wanted.jpg",
         name: "Double Knee Lose-Fit",
-        price: "1235"
+        price: "PRICE: 50$"
     },
     {
+        id: 6,
         type: "pant",
-        src: "images/maha_wanted.jpg",
+        src1: "images/sdu.jpg",
         name: "Double Knee Lose-Fit",
-        price: "1235"
+        price: "PRICE: 60$"
     }
 ]
 
-for( let i = 0; i < temp1.length; i++ ) {
-    console.log(temp1[i].image)
+let arrays_goods_pants = [
+    {
+        id: 1,
+        type: "pant",
+        src1: "images/Dickies Loose Fit Double Knee Work Pant Black.jpg",
+        src2: "images/Dickies Loose Fit Double Knee Work Pant Black 2.jpg",
+        src3: "images/Dickies Loose Fit Double Knee Work Pant Black 3.jpg",
+        name: "Double Knee Lose-Fit",
+        price: "PRICE: 1235"
+
+    },
+    {
+        id: 2,
+        type: "pant",
+        src1: "images/Dickies Loose Fit Double Knee Work Pant Khaki.jpg",
+        src2: "images/Dickies Loose Fit Double Knee Work Pant Khaki 2.jpg",
+        src3: "images/Dickies Loose Fit Double Knee Work Pant Khaki 3.jpg",
+        name: "Double Knee Lose-Fit",
+        price: "PRICE: 1235"
+    },
+    {
+        id: 3,
+        type: "pant",
+        src1: "images/Dickies Original 874 Work Pant Black.jpg",
+        src2: "images/Dickies Original 874 Work Pant Black 2.jpg",
+        src3: "images/Dickies Original 874 Work Pant Black 3.jpg",
+        name: "Dickies Men's Original 874 Work Pant",
+        price: "PRICE: 1235"
+    },
+    {
+        id: 4,
+        type: "pant",
+        src1: "images/Dickies Original 874 Work Pant Khaki.jpg",
+        src2: "images/Dickies Original 874 Work Pant Khaki 2.jpg",
+        src3: "images/Dickies Original 874 Work Pant Khaki 3.jpg",
+        name: "Double Knee Lose-Fit",
+        price: "PRICE: 1235"
+    },
+    {
+        id: 5,
+        type: "pant",
+        src1: "images/Red Kap Industrial Cargo Pant Black.jpg",
+        src2: "images/Red Kap Industrial Cargo Pant Black 2.jpg",
+        src3: "images/Red Kap Industrial Cargo Pant Black 3.jpg",
+        name: "Double Knee Lose-Fit",
+        price: "PRICE: 1235"
+    },
+    {
+        id: 6,
+        type: "pant",
+        src1: "images/Red Kap Industrial Cargo Pant Khaki.jpg",
+        src2: "images/Red Kap Industrial Cargo Pant Khaki 2.jpg",
+        src3: "images/Red Kap Industrial Cargo Pant Khaki 2.jpg",
+        name: "Double Knee Lose-Fit",
+        price: "PRICE: 1235"
+    }
+]
+
+let arrays_goods_t_shirts = [
+    {
+        id: 1,
+        type: "pant",
+        src1: "images/Dickies Short Sleeve Pocket Tee Big-tall Black.jpg",
+        name: "Double Knee Lose-Fit",
+        price: "PRICE: 1235"
+
+    },
+    {
+        id: 2,
+        type: "pant",
+        src1: "images/Dickies Short Sleeve Pocket Tee Big-tall White.jpg",
+        name: "Double Knee Lose-Fit",
+        price: "PRICE: 1235"
+    },
+    {
+        id: 3,
+        type: "pant",
+        src1: "images/Dickies Big & Tall Short Sleeve Tri-Color Logo Graphic T-Shirt Black.jpg",
+        name: "Double Knee Lose-Fit",
+        price: "PRICE: 1235"
+    },
+    {
+        id: 4,
+        type: "pant",
+        src1: "images/Dickies Men's Big & Tall Short Sleeve Tri-Color Logo Graphic T-Shirt Gray.jpg",
+        name: "Double Knee Lose-Fit",
+        price: "PRICE: 1235"
+    },
+    {
+        id: 5,
+        type: "pant",
+        src1: "images/Dickies Short Sleeve Heavyweight Crew Neck Black.jpg",
+        name: "Double Knee Lose-Fit",
+        price: "PRICE: 1235"
+    },
+    {
+        id: 6,
+        type: "pant",
+        src1: "images/Dickies Short Sleeve Heavyweight Crew Neck White.jpg",
+        name: "Double Knee Lose-Fit",
+        price: "PRICE: 1235"
+    }
+]
+
+let arrays_goods_hoodies = [
+    {
+        id: 1,
+        type: "pant",
+        src1: "images/Dickies Big Tall Pullover Fleece Hoodie.jpg",
+        name: "Double Knee Lose-Fit",
+        price: "PRICE: 1235"
+
+    },
+    {
+        id: 2,
+        type: "pant",
+        src1: "images/Dickies Full Zip Fleece Hoodie, Black, Medium.jpg",
+        name: "Double Knee Lose-Fit",
+        price: "PRICE: 1235"
+    },
+    {
+        id: 3,
+        type: "pant",
+        src1: "images/Dickies Big and Tall Wordmark DWR Pullover Fleece.jpg",
+        name: "Double Knee Lose-Fit",
+        price: "PRICE: 1235"
+    },
+    {
+        id: 4,
+        type: "pant",
+        src1: "images/Dickies Graphic Fleece Hoodie.jpg",
+        name: "Double Knee Lose-Fit",
+        price: "PRICE: 1235"
+    },
+    {
+        id: 5,
+        type: "pant",
+        src1: "images/Dickies Fleece Hooded Duck Shirt Jacket with Hydroshield.jpg",
+        name: "Double Knee Lose-Fit",
+        price: "PRICE: 1235"
+    },
+    {
+        id: 6,
+        type: "pant",
+        src1: "images/Dickies Icon Graphic Fleece.jpg",
+        name: "Double Knee Lose-Fit",
+        price: "PRICE: 1235"
+    }
+]
+
+let array_for_shuffled = []
+for( let i = 0; i < 6; i++ ) {
+    array_for_shuffled[i] = i
 }
 
-let arr = document.querySelectorAll("div div section div div div img")
-for( let i = 0; i < arr.length; i++ ) {
-    console.log(arr[i])
-}
-myfunction()
+let array_index = [];
+
+let arr_img = document.getElementsByClassName("goods_img")
+let arr_text = document.getElementsByTagName("h2")
+let arr_price = document.getElementsByTagName("h4")
 
 function myfunction() {
-    for( let i = 0; i < arr.length; i++ ) {
-        arr[i].src = temp1[i].image
+    array_index = array_for_shuffled.sort(function () {
+        return Math.random() - 0.5;
+    });
+
+    for( let i = 0; i < 6; i++ ) {
+        arr_img[i].src = arrays_goods_pop[array_index[i]].src1
+        arr_text[i].textContent = arrays_goods_pop[array_index[i]].name
+        arr_price[i].textContent = arrays_goods_pop[array_index[i]].price
+
+        arr_img[i+6].src = arrays_goods_pants[array_index[i]].src1
+        arr_text[i+6].textContent  = arrays_goods_pants[array_index[i]].name
+        arr_price[i+6].textContent  = arrays_goods_pants[array_index[i]].price
+
+        arr_img[i+12].src = arrays_goods_t_shirts[array_index[i]].src1
+        arr_text[i+12].textContent  = arrays_goods_t_shirts[array_index[i]].name
+        arr_price[i+12].textContent  = arrays_goods_t_shirts[array_index[i]].price
+
+        arr_img[i+18].src = arrays_goods_hoodies[array_index[i]].src1
+        arr_text[i+18].textContent  = arrays_goods_hoodies[array_index[i]].name
+        arr_price[i+18].textContent  = arrays_goods_hoodies[array_index[i]].price
     }
+}
+
+function loadGoods() {
+    document.getElementsByClassName("wertwert")[0].src = localStorage.getItem('id1')
+    document.getElementsByClassName("wertwert")[1].src = localStorage.getItem('id2')
+    document.getElementsByClassName("wertwert")[2].src = localStorage.getItem('id3')
+}
+
+function open_img(n) {
+    localStorage.setItem('id1', arrays_goods_pants[array_index[n%6]].src1)
+    localStorage.setItem('id2', arrays_goods_pants[array_index[n%6]].src2)
+    localStorage.setItem('id3', arrays_goods_pants[array_index[n%6]].src3)
+}
+
+// LINKS
+function qwerty1(a, b) {
+    for( let i = a; i < b; i++ ) document.getElementsByClassName("header_icon")[i].style = "fill: #ffffff"
+}
+function qwerty(a, b) {
+    for( let i = a; i < b; i++ ) document.getElementsByClassName("header_icon")[i].style = "fill: #ff0000"
+}
+
+// LIKE
+let arrays_boo_like = [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true]
+function like(n) {
+    let tempp = document.getElementsByClassName("heart_img")[n]
+    console.log(tempp.length)
+    if( tempp.src == "https://a.lmcdn.ru/static/22.12.15_1/assets/heart.99c595b3.svg" ) {
+        tempp.src = "https://a.lmcdn.ru/static/22.12.15_1/assets/heart.99c595b3.svg"
+    }
+    else {
+        tempp.src = "https://a.lmcdn.ru/static/22.12.15_1/assets/heart_selected.41219963.svg"
+    }
+    arrays_boo_like[n] = !arrays_boo_like[n]
+}
+
+function qweqwe(n) {
+    if( arrays_boo_like[n] ) {
+        let tempp = document.getElementsByClassName("heart_img")[n]
+        if( tempp.src == "https://a.lmcdn.ru/static/22.12.15_1/assets/heart_selected.41219963.svg" ) {
+            tempp.src = "https://a.lmcdn.ru/static/22.12.15_1/assets/heart.99c595b3.svg"
+        }
+        else {
+            tempp.src = "https://a.lmcdn.ru/static/22.12.15_1/assets/heart_selected.41219963.svg"
+        }
+    }
+}
+
+let boo_sign_up = true
+function sign_up() {
+    if( boo_sign_up ) document.getElementById('sign_up_img').style = "display: flex;"
+    else document.getElementById('sign_up_img').style = "display: none;"
+    boo_sign_up = !boo_sign_up
 }
