779492703c740de1778e28e0688d950f88d9ea78
Kenesbay Dimash <viport19@gmail.com>
2022-12-21 10:23:06 +0600

    fill

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@ var products = [
     id: 1,
     type: "pant",
     photo: "images/Dickies Loose Fit Double Knee Work Pant Black.jpg",
-    name: "Loose Fit Double Knee",
+    name: "Loose ",
     color: "Black",
     price: 50
 },
@@ -11,7 +11,7 @@ var products = [
     id: 2,
     type: "pant",
     photo: "images/Dickies Loose Fit Double Knee Work Pant Khaki.jpg",
-    name: "Loose Fit Double Knee",
+    name: "Loose ",
     color: "Khaki",
     price: 50
 },
@@ -19,7 +19,7 @@ var products = [
     id: 3,
     type: "pant",
     photo: "images/Dickies Original 874 Work Pant Black.jpg",
-    name: "Original 874 Work Pant",
+    name: "Original ",
     color: "Black",
     price: 65
 },
@@ -27,7 +27,7 @@ var products = [
     id: 4,
     type: "pant",
     photo: "images/Dickies Original 874 Work Pant Khaki.jpg",
-    name: "Original 874 Work Pant",
+    name: "Original ",
     color: "Khaki",
     price: 65
 },
@@ -35,7 +35,7 @@ var products = [
     id: 5,
     type: "pant",
     photo: "images/Red Kap Industrial Cargo Pant Black.jpg",
-    name: "Red Kap Industrial Cargo Pant",
+    name: "Red Kap ",
     color: "Black",
     price: 80
 },
@@ -43,7 +43,7 @@ var products = [
     id: 6,
     type: "pant",
     photo: "images/Red Kap Industrial Cargo Pant Khaki.jpg",
-    name: "Red Kap Industrial Cargo Pant",
+    name: "Red Kap ",
     color: "Khaki",
     price: 80
 },
@@ -51,7 +51,7 @@ var products = [
     id: 7,
     type: "t_shirt",
     photo: "images/Dickies Short Sleeve Pocket Tee Big-tall Black.jpg",
-    name: "Short Sleeve Pocket",
+    name: "Short ",
     color: "Black",
     price: 40
 
@@ -60,7 +60,7 @@ var products = [
     id: 8,
     type: "t_shirt",
     photo: "images/Dickies Short Sleeve Pocket Tee Big-tall White.jpg",
-    name: "Short Sleeve Pocket",
+    name: "Short ",
     color: "Black",
     price: 40
 },
@@ -68,7 +68,7 @@ var products = [
     id: 9,
     type: "t_shirt",
     photo: "images/Dickies Big & Tall Short Sleeve Tri-Color Logo Graphic T-Shirt Black.jpg",
-    name: "Big & Tall Short Sleeve",
+    name: "Big & Tall ",
     color: "Black",
     price: 25
 },
@@ -76,7 +76,7 @@ var products = [
     id: 10,
     type: "t_shirt",
     photo: "images/Dickies Men's Big & Tall Short Sleeve Tri-Color Logo Graphic T-Shirt Gray.jpg",
-    name: "Big & Tall Short Sleeve",
+    name: "Big & ",
     color: "Gray",
     price: 25
 },
@@ -84,7 +84,7 @@ var products = [
     id: 11,
     type: "t_shirt",
     photo: "images/Dickies Short Sleeve Heavyweight Crew Neck Black.jpg",
-    name: "Dickies Short Sleeve",
+    name: "Dickies ",
     color: "Black",
     price: 15
 },
@@ -92,7 +92,7 @@ var products = [
     id: 12,
     type: "t_shirt",
     photo: "images/Dickies Short Sleeve Heavyweight Crew Neck White.jpg",
-    name: "Dickies Short Sleeve",
+    name: "Dickies ",
     color: "White",
     price: 15
 },
@@ -100,7 +100,7 @@ var products = [
     id: 13,
     type: "hoodie",
     photo: "images/Dickies Big Tall Pullover Fleece Hoodie.jpg",
-    name: "Big Tall Pullover Fleece Hoodie",
+    name: "Big Tall ",
     color: "Black",
     price: 35
 
@@ -109,7 +109,7 @@ var products = [
     id: 14,
     type: "hoodie",
     photo: "images/Dickies Full Zip Fleece Hoodie, Black, Medium.jpg",
-    name: "Full Zip Fleece Hoodie",
+    name: "Full Zip ",
     color: "Black",
     price: 35
 },
@@ -117,7 +117,7 @@ var products = [
     id: 15,
     type: "hoodie",
     photo: "images/Dickies Big and Tall Wordmark DWR Pullover Fleece.jpg",
-    name: "Big and Tall Wordmark DWR Pullover Fleece",
+    name: "Big and ",
     color: "Gray",
     price: 60
 },
@@ -125,7 +125,7 @@ var products = [
     id: 16,
     type: "hoodie",
     photo: "images/Dickies Graphic Fleece Hoodie.jpg",
-    name: "Graphic Fleece Hoodie",
+    name: "Graphic ",
     color: "Gray",
     price: 45
 },
@@ -133,7 +133,7 @@ var products = [
     id: 17,
     type: "hoodie",
     photo: "images/Dickies Icon Graphic Fleece Black.jpg",
-    name: "Icon Graphic Fleece",
+    name: "Icon ",
     color: "Black",
     price: 70
 },
@@ -141,7 +141,7 @@ var products = [
     id: 18,
     type: "hoodie",
     photo: "images/Dickies Icon Graphic Fleece Gray.jpg",
-    name: "Icon Graphic Fleece",
+    name: "Icon ",
     color: "Gray",
     price: 70
 }
@@ -179,16 +179,21 @@ function loadProducts() {
 }
 loadProducts();
 function makeProduct(item) {
-return '<li>' +
-            '<div class="product-card">' +
-                '<div class="product">' +
-                    '<img class="" src="' + item.photo + '" alt="Product Photo">' +
+return '<div onmouseover="like_ heart()" class="goods">' +
+            '<div class="product">' +
+                '<div class="icon">' +
+                    '<svg class="svg-icon" viewBox="0 0 20 20">' +
+                        '<path fill="#ff0000" d="M13.22,2.984c-1.125,0-2.504,0.377-3.53,1.182C8.756,3.441,7.502,2.984,6.28,2.984c-2.6,0-4.714,2.116-4.714,4.716c0,0.32,0.032,0.644,0.098,0.96c0.799,4.202,6.781,7.792,7.46,8.188c0.193,0.111,0.41,0.168,0.627,0.168c0.187,0,0.376-0.041,0.55-0.127c0.011-0.006,1.349-0.689,2.91-1.865c0.021-0.016,0.043-0.031,0.061-0.043c0.021-0.016,0.045-0.033,0.064-0.053c3.012-2.309,4.6-4.805,4.6-7.229C17.935,5.1,15.819,2.984,13.22,2.984z M12.544,13.966c-0.004,0.004-0.018,0.014-0.021,0.018s-0.018,0.012-0.023,0.016c-1.423,1.076-2.674,1.734-2.749,1.771c0,0-6.146-3.576-6.866-7.363C2.837,8.178,2.811,7.942,2.811,7.7c0-1.917,1.554-3.47,3.469-3.47c1.302,0,2.836,0.736,3.431,1.794c0.577-1.121,2.161-1.794,3.509-1.794c1.914,0,3.469,1.553,3.469,3.47C16.688,10.249,14.474,12.495,12.544,13.966z"></path >' +
+                    '</svg>' +
                 '</div>' +
-                '<p class="probuct-card-title">' + item.name + ' ' + item.color + '</p>' +
-                '<p class="probuct-card-price">' + item.price + '$</p>' +
+                '<img class="" src="' + item.photo + '" alt="Goods Photo">' +
+                '<p>' + item.name + ' ' + item.color + '</p>' +
+                '<p>' + item.price + '$</p>' +
             '</div>' +
-        '</li>';
+        '</div>';
 }
+
+like_heart()
 
 function shuffleArray(array) {
 for (let i = array.length - 1; i > 0; i--) {
