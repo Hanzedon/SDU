ce7726cd88ede578e4a05d1467b43817a715f6cc
Kenesbay Dimash <viport19@gmail.com>
2022-12-21 08:11:04 +0600

    sui

--- src/js/script.js
+++ src/js/script.js
@@ -1,319 +1,226 @@
-    let arrays_goods_pop = [
-    {
-        id: 1,
-        type: "pant",
-        src1: "images/bagi.jpg",
-        name: "Double Knee Lose-Fit",
-        price: 10
+var products = [
+{
+    id: 1,
+    type: "pant",
+    photo: "images/Dickies Loose Fit Double Knee Work Pant Black.jpg",
+    name: "Loose Fit Double Knee",
+    color: "Black",
+    price: 50
+},
+{
+    id: 2,
+    type: "pant",
+    photo: "images/Dickies Loose Fit Double Knee Work Pant Khaki.jpg",
+    name: "Loose Fit Double Knee",
+    color: "Khaki",
+    price: 50
+},
+{
+    id: 3,
+    type: "pant",
+    photo: "images/Dickies Original 874 Work Pant Black.jpg",
+    name: "Original 874 Work Pant",
+    color: "Black",
+    price: 65
+},
+{
+    id: 4,
+    type: "pant",
+    photo: "images/Dickies Original 874 Work Pant Khaki.jpg",
+    name: "Original 874 Work Pant",
+    color: "Khaki",
+    price: 65
+},
+{
+    id: 5,
+    type: "pant",
+    photo: "images/Red Kap Industrial Cargo Pant Black.jpg",
+    name: "Red Kap Industrial Cargo Pant",
+    color: "Black",
+    price: 80
+},
+{
+    id: 6,
+    type: "pant",
+    photo: "images/Red Kap Industrial Cargo Pant Khaki.jpg",
+    name: "Red Kap Industrial Cargo Pant",
+    color: "Khaki",
+    price: 80
+},
+{
+    id: 7,
+    type: "t_shirt",
+    photo: "images/Dickies Short Sleeve Pocket Tee Big-tall Black.jpg",
+    name: "Short Sleeve Pocket",
+    color: "Black",
+    price: 40
 
-    },
-    {
-        id: 2,
-        type: "pant",
-        src1: "images/muha.jpg",
-        name: "Double Knee Lose-Fit",
-        price: 20
-    },
-    {
-        id: 3,
-        type: "pant",
-        src1: "images/muha.jpg",
-        name: "Double Knee Lose-Fit",
-        price: 30
-    },
-    {
-        id: 4,
-        type: "pant",
-        src1: "images/muha.jpg",
-        name: "Double Knee Lose-Fit",
-        price: 40
-    },
-    {
-        id: 5,
-        type: "pant",
-        src1: "images/maha_wanted.jpg",
-        name: "Double Knee Lose-Fit",
-        price: 50
-    },
-    {
-        id: 6,
-        type: "pant",
-        src1: "images/sdu.jpg",
-        name: "Double Knee Lose-Fit",
-        price: 60
-    }
-]
+},
+{
+    id: 8,
+    type: "t_shirt",
+    photo: "images/Dickies Short Sleeve Pocket Tee Big-tall White.jpg",
+    name: "Short Sleeve Pocket",
+    color: "Black",
+    price: 40
+},
+{
+    id: 9,
+    type: "t_shirt",
+    photo: "images/Dickies Big & Tall Short Sleeve Tri-Color Logo Graphic T-Shirt Black.jpg",
+    name: "Big & Tall Short Sleeve",
+    color: "Black",
+    price: 25
+},
+{
+    id: 10,
+    type: "t_shirt",
+    photo: "images/Dickies Men's Big & Tall Short Sleeve Tri-Color Logo Graphic T-Shirt Gray.jpg",
+    name: "Big & Tall Short Sleeve",
+    color: "Gray",
+    price: 25
+},
+{
+    id: 11,
+    type: "t_shirt",
+    photo: "images/Dickies Short Sleeve Heavyweight Crew Neck Black.jpg",
+    name: "Dickies Short Sleeve",
+    color: "Black",
+    price: 15
+},
+{
+    id: 12,
+    type: "t_shirt",
+    photo: "images/Dickies Short Sleeve Heavyweight Crew Neck White.jpg",
+    name: "Dickies Short Sleeve",
+    color: "White",
+    price: 15
+},
+{
+    id: 13,
+    type: "hoodie",
+    photo: "images/Dickies Big Tall Pullover Fleece Hoodie.jpg",
+    name: "Big Tall Pullover Fleece Hoodie",
+    color: "Black",
+    price: 35
 
-let arrays_goods_pants = [
-    {
-        id: 1,
-        type: "pant",
-        src1: "images/Dickies Loose Fit Double Knee Work Pant Black.jpg",
-        src2: "images/Dickies Loose Fit Double Knee Work Pant Black 2.jpg",
-        src3: "images/Dickies Loose Fit Double Knee Work Pant Black 3.jpg",
-        name: "Loose Fit Double Knee",
-        color: "Black",
-        price: 50
-    },
-    {
-        id: 2,
-        type: "pant",
-        src1: "images/Dickies Loose Fit Double Knee Work Pant Khaki.jpg",
-        src2: "images/Dickies Loose Fit Double Knee Work Pant Khaki 2.jpg",
-        src3: "images/Dickies Loose Fit Double Knee Work Pant Khaki 3.jpg",
-        name: "Loose Fit Double Knee",
-        color: "Khaki",
-        price: 50
-    },
-    {
-        id: 3,
-        type: "pant",
-        src1: "images/Dickies Original 874 Work Pant Black.jpg",
-        src2: "images/Dickies Original 874 Work Pant Black 2.jpg",
-        src3: "images/Dickies Original 874 Work Pant Black 3.jpg",
-        name: "Original 874 Work Pant",
-        color: "Black",
-        price: 65
-    },
-    {
-        id: 4,
-        type: "pant",
-        src1: "images/Dickies Original 874 Work Pant Khaki.jpg",
-        src2: "images/Dickies Original 874 Work Pant Khaki 2.jpg",
-        src3: "images/Dickies Original 874 Work Pant Khaki 3.jpg",
-        name: "Original 874 Work Pant",
-        color: "Khaki",
-        price: 65
-    },
-    {
-        id: 5,
-        type: "pant",
-        src1: "images/Red Kap Industrial Cargo Pant Black.jpg",
-        src2: "images/Red Kap Industrial Cargo Pant Black 2.jpg",
-        src3: "images/Red Kap Industrial Cargo Pant Black 3.jpg",
-        name: "Red Kap Industrial Cargo Pant",
-        color: "Black",
-        price: 80
-    },
-    {
-        id: 6,
-        type: "pant",
-        src1: "images/Red Kap Industrial Cargo Pant Khaki.jpg",
-        src2: "images/Red Kap Industrial Cargo Pant Khaki 2.jpg",
-        src3: "images/Red Kap Industrial Cargo Pant Khaki 2.jpg",
-        name: "Red Kap Industrial Cargo Pant",
-        color: "Khaki",
-        price: 80
-    }
-]
-
-let arrays_goods_t_shirts = [
-    {
-        id: 1,
-        type: "pant",
-        src1: "images/Dickies Short Sleeve Pocket Tee Big-tall Black.jpg",
-        src2: "images/Dickies Short Sleeve Pocket Tee Big-tall Black 2.jpg",
-        src3: "images/Dickies Short Sleeve Pocket Tee Big-tall Black 3.jpg",
-        name: "Short Sleeve Pocket",
-        color: "Black",
-        price: 40
-
-    },
-    {
-        id: 2,
-        type: "pant",
-        src1: "images/Dickies Short Sleeve Pocket Tee Big-tall White.jpg",
-        src2: "images/Dickies Short Sleeve Pocket Tee Big-tall White 2.jpg",
-        src3: "images/Dickies Short Sleeve Pocket Tee Big-tall White 3.jpg",
-        name: "Short Sleeve Pocket",
-        color: "Black",
-        price: 40
-    },
-    {
-        id: 3,
-        type: "pant",
-        src1: "images/Dickies Big & Tall Short Sleeve Tri-Color Logo Graphic T-Shirt Black.jpg",
-        src2: "images/Dickies Big & Tall Short Sleeve Tri-Color Logo Graphic T-Shirt Black 2.jpg",
-        src3: "images/Dickies Big & Tall Short Sleeve Tri-Color Logo Graphic T-Shirt Black 3.jpg",
-        name: "Big & Tall Short Sleeve",
-        color: "Black",
-        price: 25
-    },
-    {
-        id: 4,
-        type: "pant",
-        src1: "images/Dickies Men's Big & Tall Short Sleeve Tri-Color Logo Graphic T-Shirt Gray.jpg",
-        src2: "images/Dickies Men's Big & Tall Short Sleeve Tri-Color Logo Graphic T-Shirt Gray 2.jpg",
-        src3: "images/Dickies Men's Big & Tall Short Sleeve Tri-Color Logo Graphic T-Shirt Gray 3.jpg",
-        name: "Big & Tall Short Sleeve",
-        color: "Gray",
-        price: 25
-    },
-    {
-        id: 5,
-        type: "pant",
-        src1: "images/Dickies Short Sleeve Heavyweight Crew Neck Black.jpg",
-        src2: "images/Dickies Short Sleeve Heavyweight Crew Neck Black 2.jpg",
-        src3: "images/Dickies Short Sleeve Heavyweight Crew Neck Black 3.jpg",
-        name: "Dickies Short Sleeve",
-        color: "Black",
-        price: 15
-    },
-    {
-        id: 6,
-        type: "pant",
-        src1: "images/Dickies Short Sleeve Heavyweight Crew Neck White.jpg",
-        src2: "images/Dickies Short Sleeve Heavyweight Crew Neck White 2.jpg",
-        src3: "images/Dickies Short Sleeve Heavyweight Crew Neck White 3.jpg",
-        name: "Dickies Short Sleeve",
-        color: "White",
-        price: 15
-    }
-]
-
-let arrays_goods_hoodies = [
-    {
-        id: 1,
-        type: "pant",
-        src1: "images/Dickies Big Tall Pullover Fleece Hoodie.jpg",
-        src2: "images/Dickies Big Tall Pullover Fleece Hoodie 2.jpg",
-        src3: "images/Dickies Big Tall Pullover Fleece Hoodie.jpg",
-        name: "Big Tall Pullover Fleece Hoodie",
-        color: "Black",
-        price: 35
-
-    },
-    {
-        id: 2,
-        type: "pant",
-        src1: "images/Dickies Full Zip Fleece Hoodie, Black, Medium.jpg",
-        src2: "images/Dickies Full Zip Fleece Hoodie, Black, Medium 2.jpg",
-        src3: "images/Dickies Full Zip Fleece Hoodie, Black, Medium.jpg",
-        name: "Full Zip Fleece Hoodie",
-        color: "Black",
-        price: 35
-    },
-    {
-        id: 3,
-        type: "pant",
-        src1: "images/Dickies Big and Tall Wordmark DWR Pullover Fleece.jpg",
-        src2: "images/Dickies Big and Tall Wordmark DWR Pullover Fleece 2.jpg",
-        src3: "images/Dickies Big and Tall Wordmark DWR Pullover Fleece 3.jpg",
-        name: "Big and Tall Wordmark DWR Pullover Fleece",
-        color: "Gray",
-        price: 60
-    },
-    {
-        id: 4,
-        type: "pant",
-        src1: "images/Dickies Graphic Fleece Hoodie.jpg",
-        src2: "images/Dickies Graphic Fleece Hoodie 2.jpg",
-        src3: "images/Dickies Graphic Fleece Hoodie.jpg",
-        name: "Graphic Fleece Hoodie",
-        color: "Gray",
-        price: 45
-    },
-    {
-        id: 5,
-        type: "pant",
-        src1: "images/Dickies Icon Graphic Fleece Black.jpg",
-        src2: "images/Dickies Icon Graphic Fleece Black 2.jpg",
-        src3: "images/Dickies Icon Graphic Fleece Black 3.jpg",
-        name: "Icon Graphic Fleece",
-        color: "Black",
-        price: 70
-    },
-    {
-        id: 6,
-        type: "pant",
-        src1: "images/Dickies Icon Graphic Fleece Gray.jpg",
-        src2: "images/Dickies Icon Graphic Fleece Gray 2.jpg",
-        src3: "images/Dickies Icon Graphic Fleece Gray 3.jpg",
-        name: "Icon Graphic Fleece",
-        color: "Gray",
-        price: 70
-    }
-]
-
-let array_for_shuffled = []
-for( let i = 0; i < 6; i++ ) {
-    array_for_shuffled[i] = i
+},
+{
+    id: 14,
+    type: "hoodie",
+    photo: "images/Dickies Full Zip Fleece Hoodie, Black, Medium.jpg",
+    name: "Full Zip Fleece Hoodie",
+    color: "Black",
+    price: 35
+},
+{
+    id: 15,
+    type: "hoodie",
+    photo: "images/Dickies Big and Tall Wordmark DWR Pullover Fleece.jpg",
+    name: "Big and Tall Wordmark DWR Pullover Fleece",
+    color: "Gray",
+    price: 60
+},
+{
+    id: 16,
+    type: "hoodie",
+    photo: "images/Dickies Graphic Fleece Hoodie.jpg",
+    name: "Graphic Fleece Hoodie",
+    color: "Gray",
+    price: 45
+},
+{
+    id: 17,
+    type: "hoodie",
+    photo: "images/Dickies Icon Graphic Fleece Black.jpg",
+    name: "Icon Graphic Fleece",
+    color: "Black",
+    price: 70
+},
+{
+    id: 18,
+    type: "hoodie",
+    photo: "images/Dickies Icon Graphic Fleece Gray.jpg",
+    name: "Icon Graphic Fleece",
+    color: "Gray",
+    price: 70
 }
+];
 
-let array_index = [];
+console.log('dusmkad');
 
-let arr_img = document.getElementsByClassName("goods_img")
-let arr_text = document.getElementsByTagName("h2")
-let arr_price = document.getElementsByTagName("h4")
+function loadProducts() {
+    let pants = '';
+    let t_shirts = '';
+    let hoodies = '';
+    let add = '';
 
-function myfunction() {
-    array_index = array_for_shuffled.sort(function () {
-        return Math.random() - 0.5;
+    console.log('dusmkad');
+    shuffleArray(products);
+    products.forEach((product) => {
+        add = makeProduct(product);
+        console.log(add);
+        switch (product.type) {
+            case 'pant':
+                pants += add;
+                break;
+            case 't_shirt':
+                t_shirts += add;
+                break;
+            case 'hoodie':
+                hoodies += add;
+                break;
+        }
     });
 
-    for( let i = 0; i < 6; i++ ) {
-        arr_img[i].src = arrays_goods_pop[array_index[i]].src1
-        arr_text[i].textContent = arrays_goods_pop[array_index[i]].name
-        arr_price[i].textContent = 'PRICE: ' + arrays_goods_pop[array_index[i]].price + '$'
-
-        arr_img[i+6].src = arrays_goods_pants[array_index[i]].src1
-        arr_text[i+6].textContent  = arrays_goods_pants[array_index[i]].name
-        arr_price[i+6].textContent  = 'PRICE: ' + arrays_goods_pants[array_index[i]].price + '$'
-
-        arr_img[i+12].src = arrays_goods_t_shirts[array_index[i]].src1
-        arr_text[i+12].textContent  = arrays_goods_t_shirts[array_index[i]].name
-        arr_price[i+12].textContent  = 'PRICE: ' + arrays_goods_t_shirts[array_index[i]].price + '$'
-
-        arr_img[i+18].src = arrays_goods_hoodies[array_index[i]].src1
-        arr_text[i+18].textContent  = arrays_goods_hoodies[array_index[i]].name
-        arr_price[i+18].textContent  = 'PRICE: ' + arrays_goods_hoodies[array_index[i]].price + '$'
-    }
+    $("#pants").append(pants);
+    $('#t_shirts').append(t_shirts);
+    $('#hoodies').append(hoodies);
+}
+loadProducts();
+function makeProduct(item) {
+return '<li>' +
+            '<div class="product-card">' +
+                '<div class="product">' +
+                    '<img class="" src="' + item.photo + '" alt="Product Photo">' +
+                '</div>' +
+                '<p class="probuct-card-title">' + item.name + ' ' + item.color + '</p>' +
+                '<p class="probuct-card-price">' + item.price + '$</p>' +
+            '</div>' +
+        '</li>';
 }
 
-function loadGoods() {
-    document.getElementsByClassName("wertwert")[0].src = localStorage.getItem('id1')
-    document.getElementsByClassName("wertwert")[1].src = localStorage.getItem('id2')
-    document.getElementsByClassName("wertwert")[2].src = localStorage.getItem('id3')
+function shuffleArray(array) {
+for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+}
 }
 
-function open_img(n) {
-    if( n < 12 ) {
-        localStorage.setItem('id1', arrays_goods_pants[array_index[n%6]].src1)
-        localStorage.setItem('id2', arrays_goods_pants[array_index[n%6]].src2)
-        localStorage.setItem('id3', arrays_goods_pants[array_index[n%6]].src3)
-    }
-    else if( n < 18 ) {
-        localStorage.setItem('id1', arrays_goods_t_shirts[array_index[n%6]].src1)
-        localStorage.setItem('id2', arrays_goods_t_shirts[array_index[n%6]].src2)
-        localStorage.setItem('id3', arrays_goods_t_shirts[array_index[n%6]].src3)
-    }
-    else if( n < 24 ) {
-        localStorage.setItem('id1', arrays_goods_hoodies[array_index[n%6]].src1)
-        localStorage.setItem('id2', arrays_goods_hoodies[array_index[n%6]].src2)
-        localStorage.setItem('id3', arrays_goods_hoodies[array_index[n%6]].src3)
-    }
-}
+// '<div class="add-button">' +
+//     '<button onclick="addToCart(' + item.id + ')">Add to Cart</button>' +
+// '</div>' +
 
 // LINKS
 function qwerty1(a, b) {
-    for( let i = a; i < b; i++ ) document.getElementsByClassName("header_icon")[i].style = "fill: #ffffff"
+for( let i = a; i < b; i++ ) document.getElementsByClassName("header_icon")[i].style = "fill: #ffffff"
 }
 function qwerty(a, b) {
-    for( let i = a; i < b; i++ ) document.getElementsByClassName("header_icon")[i].style = "fill: #ff0000"
+for( let i = a; i < b; i++ ) document.getElementsByClassName("header_icon")[i].style = "fill: #ff0000"
 }
 
 // LIKE
 let arrays_boo_like = [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true]
 
 function like(n) {
-    let tempp = document.getElementsByClassName("heart_img")[n]
-    if( tempp.src == "https://a.lmcdn.ru/static/22.12.15_1/assets/heart.99c595b3.svg" ) {
-        tempp.src = "https://a.lmcdn.ru/static/22.12.15_1/assets/heart.99c595b3.svg"
-    }
-    else {
-        tempp.src = "https://a.lmcdn.ru/static/22.12.15_1/assets/heart_selected.41219963.svg"
-    }
-    arrays_boo_like[n] = !arrays_boo_like[n]
+let tempp = document.getElementsByClassName("heart_img")[n]
+if( tempp.src == "https://a.lmcdn.ru/static/22.12.15_1/assets/heart.99c595b3.svg" ) {
+    tempp.src = "https://a.lmcdn.ru/static/22.12.15_1/assets/heart.99c595b3.svg"
+}
+else {
+    tempp.src = "https://a.lmcdn.ru/static/22.12.15_1/assets/heart_selected.41219963.svg"
+}
+arrays_boo_like[n] = !arrays_boo_like[n]
 }
 
 function qweqwe(n) {
@@ -329,8 +236,8 @@ function qweqwe(n) {
 }
 
 let boo_sign_in = true
-function sign_in() {
-    if( boo_sign_in ) document.getElementById('sign_in_img').style = "display: flex;"
-    else document.getElementById('sign_in_img').style = "display: none;"
+    function sign_in() {
+    if( boo_sign_in ) document.getElementById('sign_in_close').style = "display: flex;"
+    else document.getElementById('sign_in_close').style = "display: none;"
     boo_sign_in = !boo_sign_in
 }
