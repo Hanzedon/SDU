fb94f6116e4090908a05473afc8659fdb02587bf
Kenesbay Dimash <viport19@gmail.com>
2022-12-22 08:40:02 +0600

    localStorage

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-var products = [
+let products = [
 {
     id: 1,
     type: "pant",
@@ -147,19 +147,16 @@ var products = [
 }
 ];
 
-console.log('dusmkad');
-
-function loadProducts() {
+// HOME
+function loadGoods() {
     let pants = '';
     let t_shirts = '';
     let hoodies = '';
     let add = '';
 
-    console.log('dusmkad');
     shuffleArray(products);
     products.forEach((product) => {
         add = makeProduct(product);
-        console.log(add);
         switch (product.type) {
             case 'pant':
                 pants += add;
@@ -177,71 +174,181 @@ function loadProducts() {
     $('#t_shirts').append(t_shirts);
     $('#hoodies').append(hoodies);
 }
-loadProducts();
+
 function makeProduct(item) {
-return '<div onmouseover="like_ heart()" class="goods">' +
-            '<div class="product">' +
-                '<div class="icon">' +
-                    '<svg class="svg-icon" viewBox="0 0 20 20">' +
-                        '<path fill="#ff0000" d="M13.22,2.984c-1.125,0-2.504,0.377-3.53,1.182C8.756,3.441,7.502,2.984,6.28,2.984c-2.6,0-4.714,2.116-4.714,4.716c0,0.32,0.032,0.644,0.098,0.96c0.799,4.202,6.781,7.792,7.46,8.188c0.193,0.111,0.41,0.168,0.627,0.168c0.187,0,0.376-0.041,0.55-0.127c0.011-0.006,1.349-0.689,2.91-1.865c0.021-0.016,0.043-0.031,0.061-0.043c0.021-0.016,0.045-0.033,0.064-0.053c3.012-2.309,4.6-4.805,4.6-7.229C17.935,5.1,15.819,2.984,13.22,2.984z M12.544,13.966c-0.004,0.004-0.018,0.014-0.021,0.018s-0.018,0.012-0.023,0.016c-1.423,1.076-2.674,1.734-2.749,1.771c0,0-6.146-3.576-6.866-7.363C2.837,8.178,2.811,7.942,2.811,7.7c0-1.917,1.554-3.47,3.469-3.47c1.302,0,2.836,0.736,3.431,1.794c0.577-1.121,2.161-1.794,3.509-1.794c1.914,0,3.469,1.553,3.469,3.47C16.688,10.249,14.474,12.495,12.544,13.966z"></path >' +
-                    '</svg>' +
+return  '<div class="product" onmouseover="like_heart()">' +
+            '<div class="icon-like">' +
+                '<img src="https://a.lmcdn.ru/static/22.12.19/assets/heart.99c595b3.svg"></img>' +
+            '</div>' +
+            '<div class="" onclick="saveID(' + item.id + ')">' +
+                '<div class="goods-img">' +
+                    '<img class="" src="' + item.photo + '" alt="Goods Photo">' +
                 '</div>' +
-                '<img class="" src="' + item.photo + '" alt="Goods Photo">' +
-                '<p>' + item.name + ' ' + item.color + '</p>' +
+                '<h3>' + item.name + ' ' + item.color + '</h3>' +
                 '<p>' + item.price + '$</p>' +
             '</div>' +
         '</div>';
 }
 
-like_heart()
-
 function shuffleArray(array) {
-for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-}
-}
-
-// '<div class="add-button">' +
-//     '<button onclick="addToCart(' + item.id + ')">Add to Cart</button>' +
-// '</div>' +
-
-// LINKS
-function qwerty1(a, b) {
-for( let i = a; i < b; i++ ) document.getElementsByClassName("header_icon")[i].style = "fill: #ffffff"
-}
-function qwerty(a, b) {
-for( let i = a; i < b; i++ ) document.getElementsByClassName("header_icon")[i].style = "fill: #ff0000"
-}
-
-// LIKE
-let arrays_boo_like = [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true]
-
-function like(n) {
-let tempp = document.getElementsByClassName("heart_img")[n]
-if( tempp.src == "https://a.lmcdn.ru/static/22.12.15_1/assets/heart.99c595b3.svg" ) {
-    tempp.src = "https://a.lmcdn.ru/static/22.12.15_1/assets/heart.99c595b3.svg"
-}
-else {
-    tempp.src = "https://a.lmcdn.ru/static/22.12.15_1/assets/heart_selected.41219963.svg"
-}
-arrays_boo_like[n] = !arrays_boo_like[n]
-}
-
-function qweqwe(n) {
-    if( arrays_boo_like[n] ) {
-        let tempp = document.getElementsByClassName("heart_img")[n]
-        if( tempp.src == "https://a.lmcdn.ru/static/22.12.15_1/assets/heart_selected.41219963.svg" ) {
-            tempp.src = "https://a.lmcdn.ru/static/22.12.15_1/assets/heart.99c595b3.svg"
-        }
-        else {
-            tempp.src = "https://a.lmcdn.ru/static/22.12.15_1/assets/heart_selected.41219963.svg"
-        }
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
 }
 
-let boo_sign_in = true
-    function sign_in() {
+// GOODS
+function saveID(n) {
+    localStorage.setItem('id', n-1);
+    window.location = "goods.html";
+}
+
+function loadProduct() {
+    let index = localStorage.getItem('id');
+    $("#product").append(
+                    '<div class="goods">' +
+                        '<div class="goods-img">' +
+                            '<img class="" src="' + products[index].photo + '" alt="Goods Photo">' +
+                        '</div>' +
+                        '<div class="about">' +
+                            '<h3>' + products[index].name + '</h3>' +
+                            '<p>' + products[index].price + '</p>' +
+                        '</div>' +
+                    '</div>'
+    );
+}
+
+
+function submit() {
+    let user_name = $('#user_name').val();
+    let first_name = $('#first_name').val();
+    let last_name = $('#last_name').val();
+    let email = $('#email').val();
+    let password1 = $('#password1').val();
+    let password2 = $('#password2').val();
+    let boo_regiter = true;
+    let user_array = getUsers();
+
+    if( user_name.length < 5 || user_name == '' ) {
+        document.getElementById('user_name').style = 'background-color: rgb(255, 0, 0, 0.4)';
+        document.getElementById("user_name").placeholder = "jfiejf";
+        boo_regiter = false;
+    }
+    else {
+        // let index = qwerqwer.indexOf(user_name);
+        // console.log( index );
+
+        // if( index === -1 && user_array.length > 0 ) {
+        //     console.log( "INDEXINDEX" );
+        //     document.getElementById('user_name').style = 'background-color: rgb(255, 0, 0, 0.4)';
+        //     document.getElementById("user_name").placeholder = "NickName ERROR";
+        //     boo_regiter = false;
+        // }
+        // else {
+            document.getElementById('user_name').style = 'background-color: rgb(0, 0, 0, 0.1)';
+            boo_regiter = true;
+        // }
+    }
+    if( first_name.length < 5 || first_name == '' ) {
+        document.getElementById('first_name').style = 'background-color: rgb(255, 0, 0, 0.4)';
+        document.getElementById("first_name").placeholder = "jfiejf";
+        boo_regiter = false;
+    }
+    else {
+        document.getElementById('first_name').style = 'background-color: rgb(0, 0, 0, 0.1)';
+        boo_regiter = true;
+    }
+    if( last_name.length < 5 || last_name == '' ) {
+        document.getElementById('last_name').style = 'background-color: rgb(255, 0, 0, 0.4)';
+        document.getElementById("last_name").placeholder = "jfiejf";
+        boo_regiter = false;
+    }
+    else {
+        document.getElementById('last_name').style = 'background-color: rgb(0, 0, 0, 0.1)';
+        boo_regiter = true;
+    }
+    if( email.length < 5 || email == '' || email.match('@') ) {
+        document.getElementById('email').style = 'background-color: rgb(255, 0, 0, 0.4)';
+        document.getElementById("email").placeholder = "jfiejf";
+        boo_regiter = false;
+    }
+    else {
+        document.getElementById('email').style = 'background-color: rgb(0, 0, 0, 0.1)';
+        boo_regiter = true;
+    }
+    if( password1.length < 5 || password1 == '' ) {
+        document.getElementById('password1').style = 'background-color: rgb(255, 0, 0, 0.4)';
+        document.getElementById("password1").placeholder = "jfiejf";
+        boo_regiter = false;
+    }
+    else {
+        document.getElementById('password1').style = 'background-color: rgb(0, 0, 0, 0.1)';
+        boo_regiter = true;
+    }
+    if( password2.length < 5 || password2 == '' ) {
+        document.getElementById('password2').style = 'background-color: rgb(255, 0, 0, 0.4)';
+        document.getElementById("password2").placeholder = "jfiejf";
+        boo_regiter = false;
+    }
+    else {
+        document.getElementById('password2').style = 'background-color: rgb(0, 0, 0, 0.1)';
+        boo_regiter = true;
+    }
+    if( password2 == '' || password1 != password2 ) {
+        document.getElementById('password1').style = 'background-color: rgb(255, 0, 0, 0.4)';
+        document.getElementById("password1").placeholder = "jfiejf";
+        boo_regiter = false;
+    }
+    else {
+        document.getElementById('password1').style = 'background-color: rgb(0, 0, 0, 0.1)';
+        boo_regiter = true;
+    }
+
+    // console.log( wqerqwer[0].name );
+
+    // user_array.push(
+    //     [
+    //         {
+    //             id: 1 + user_array.length,
+    //             user_name: user_name,
+    //             first_name: first_name,
+    //             last_name: last_name,
+    //             email: email,
+    //             password: password1
+    //         }
+    //     ]
+    // );
+
+    // localStorage.setItem('users', JSON.stringify(user_array));
+}
+
+// console.log( JSON.parse(localStorage.getItem('users')) );
+
+function getUsers() {
+    const temp = localStorage.getItem('users');
+    if( temp !== null ) {
+        let qweqweqwe = JSON.parse(temp);
+        console.log( JSON.parse(localStorage.getItem('users'))[0].user_name );
+        return qweqweqwe;
+    }
+    return [];
+}
+
+function setUsers() {
+    let temp = getUsers();
+    localStorage.setItem('users', JSON.stringify(temp));
+}
+
+// LINKS
+function qwerty1(a, b) {
+    for( let i = a; i < b; i++ ) document.getElementsByClassName("header_icon")[i].style = "fill: #ffffff"
+}
+function qwerty(a, b) {
+    for( let i = a; i < b; i++ ) document.getElementsByClassName("header_icon")[i].style = "fill: #ff0000"
+}
+
+let boo_sign_in = true;
+function sign_in() {
     if( boo_sign_in ) document.getElementById('sign_in_close').style = "display: flex;"
     else document.getElementById('sign_in_close').style = "display: none;"
     boo_sign_in = !boo_sign_in
